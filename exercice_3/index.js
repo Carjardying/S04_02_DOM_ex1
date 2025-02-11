98b06a57e5abe1eb36d94b94df13d216869d6657
Carjardying <carolinejardy.pro@gmail.com>
2025-02-11 15:40:14 +0100

    exercice 3 crée

---
+++ exercice_3/index.js
@@ -0,0 +1,12 @@
+/*Pour cet exercice, on va créer un bouton qui compte les cliques.
+
+Pour cela créé un <button id="counter">0 clics!</button> dans ton HTML.
+
+
+Ensuite, ajoute un script JS :
+- Récupère le bouton dans le code JavaScript
+- Créé une variable let count = 0 pour compter le nombre de clics
+- Ajoute un évènement sur le bouton avec addEventListener: 
+            - À chaque clic, incrémente la variable count
+            - puis modifie le texte à l’intérieur du bouton avec la nouvelle valeur de count*/
+
