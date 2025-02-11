66e059e6ca4a97d63e3dd529655c370fe9f0669f
Carjardying <carolinejardy.pro@gmail.com>
2025-02-11 11:41:17 +0100

    Exercice 1

---
+++ exercice_1/index.js
@@ -0,0 +1,6 @@
+// on recupère le h1 de la page
+const pageTitle = document.querySelector('h1').innerText = "On tente?";
+// on recupère l'élément avec l'id newsletter (# est utilisé pour les ids)
+/* const subscribeButton = document.querySelector('#newsletter');
+// on recupère dans un tableau tous les éléments qui ont la classe recipe
+const recipes = document.querySelectorAll('.recipe'); */
